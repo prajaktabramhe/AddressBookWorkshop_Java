e2b6ef7197b79a32b05d4bb996a2e92fb260d67f
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-23 23:10:45 +0530

    [Prajakta]: Add code

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,2 +1,35 @@
-package com.bridgelabz.addressbook;public class AddressBook {
+package com.bridgelabz.addressbook;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook
+{
+    static Scanner sc = new Scanner(System.in);
+    static List<AddressBookDetails> lst = new ArrayList<AddressBookDetails>();
+    AddressBookDetails addContact;
+
+    public void addContact()
+    {
+        System.out.println("Enter your firstName : ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter your lastName : ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter your address : ");
+        String address = sc.nextLine();
+        System.out.println("Enter your city : ");
+        String city = sc.nextLine();
+        System.out.println("Enter your state : ");
+        String state = sc.nextLine();
+        System.out.println("Enter your zipCode : ");
+        String zip = sc.nextLine();
+        System.out.println("Enter your phoneNo : ");
+        String phoneNo = sc.nextLine();
+        System.out.println("Enter your emailId : ");
+        String email = sc.nextLine();
+        addContact = new AddressBookDetails(firstName, lastName, address, city, state, zip, phoneNo, email);
+        lst.add(addContact);
+
+    }
 }
--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,9 +1,36 @@
 package com.bridgelabz.addressbook;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class AddressBookMain
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Welcome To The Address Book Problem");
+        updateAddressBook();
+    }
+    public static void updateAddressBook() throws IOException
+    {
+        AddressBook addressBook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+        int count=1;
+        while (count==1)
+        {
+            System.out.println("Welcome to address book program ");
+            System.out.println(
+                    "Enter choice \n1. AddContact ");
+            int choice = sc.nextInt();
+
+            switch (choice)
+            {
+                case 1:
+                    addressBook.addContact();
+                    count=0;
+                    break;
+                default:
+                    System.out.println(" Enter a valid choice");
+                    break;
+            }
+        }
     }
 }
