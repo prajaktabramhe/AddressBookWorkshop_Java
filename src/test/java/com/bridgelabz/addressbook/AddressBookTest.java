e29c850e18fa8ecd602094c3a100796976c3ea6c
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-03 01:12:35 +0530

    [Prajakta]: Added: Passed the multple data to test case for add multiple address book to the system

--- src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
@@ -4,48 +4,58 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class AddressBookTest
 {
+
     @Test
     public  void givenContact_WhenAddedToAddressBook_ShouldReturnTrue()
     {
       AddressBookMain addressBookMain = new AddressBookMain();
-      AddressBookDetails addressBookDetails = new AddressBookDetails("Prajakta", "Bramhe", "Ashtyavinayak colony", "Nagpur", "MH", "440034", "8149240833", "Prajkat@gmail.com");
-
-        List<AddressBookDetails> addressBookDetailsList = addressBookMain.addContact(addressBookDetails);
-      Assertions.assertEquals(1, addressBookDetailsList.size());
+      AddressBookDetails addressBookDetails = new AddressBookDetails("Prajakta", "Bramhe", "Ashtyavinayak colony", "Nagpur", "MH", "440034", "8149240833", "prajkat@gmail.com");
+      HashMap<String,List<AddressBookDetails>>  addressBookDetailsList = addressBookMain.addContact("Family", addressBookDetails);
+      Assertions.assertEquals(1, addressBookDetailsList.get("Family").size());
     }
 
     @Test
-    public  void givenContactName_whenUpdated_shoouldReturn_TRUE()
+    public void givenContactName_whenUpdated_ShouldReturn_true()
     {
         AddressBookMain addressBookMain = new AddressBookMain();
-        AddressBookDetails addressBookDetails = new AddressBookDetails("Prajakta", "Bramhe", " Ashtyavinayak Colony", "Napur", " MH", "440034", "8149240833", "prajakta@gmail.com");
-        List<AddressBookDetails> addressBookDetailsList = addressBookMain.addContact(addressBookDetails);
-        AddressBookDetails updateContact = addressBookMain.updateContact(addressBookDetailsList, "Prajakta", "address", "Ganesh colony");
-        Assertions.assertEquals("Ganesh colony", updateContact.address);
+        AddressBookDetails contact = new AddressBookDetails("Prajakta", "Bramhe", "Ashtyavinayak colony", "Nagpur", "MH", "440034", "8149240833", "prajkat@gmail.com");
+        HashMap<String, List<AddressBookDetails>> contactList = addressBookMain.addContact("Family", contact);
+        boolean result = addressBookMain.updateContact(contactList, "Family", "Prajakta", "address", "Abhyankara Nagar");
+        Assertions.assertTrue(result);
     }
 
-    @Test
+  @Test
     public void givenContactName_whenDeleted_ShouldReturn_true()
-    {
+  {
         AddressBookMain addressBookMain = new AddressBookMain();
         AddressBookDetails addressBookDetails = new AddressBookDetails("Prajakta", "Bramhe", "Lakshmi Nagar", "Nagpur", "MH", "440034", "8149240833", "prajakta14@gmail.com");
-        List<AddressBookDetails> addressBookDetailsList = addressBookMain.addContact(addressBookDetails);
-        List<AddressBookDetails> deleteContactList = addressBookMain.deleteContact(addressBookDetailsList, "Prajakta");
-        Assertions.assertEquals(0, deleteContactList.size());
+        HashMap<String, List<AddressBookDetails>> addressBookDetailsList = addressBookMain.addContact("Family", addressBookDetails);
+        boolean result = addressBookMain.deleteContact(addressBookDetailsList, "Family", "Prajakta");
+        Assertions.assertTrue(result);
     }
 
     @Test
-    public void givenListOfContacts_whenAdded_ShouldReturn_true() {
+    public void givenListOfContacts_whenAdded_ShouldReturn_true()
+    {
         AddressBookMain addressBookMain = new AddressBookMain();
         List<AddressBookDetails> addressBookDetailsList = new ArrayList<>();
-        addressBookDetailsList.add(new AddressBookDetails("Samiksha", "Shende", "Ram Nagar", "Wardha", "MH", "442001", "7385697450", "shende.samiksha@gmail.com"));
-        addressBookDetailsList.add(new AddressBookDetails("Apurva", "Yede", "Murarka Layout", "Wardha", "MH", "442001", "8794569789", "apurvayede@gmail.com"));
+        addressBookDetailsList.add(new AddressBookDetails("Prajakta", "Bramhe", " Ashtyavinayak Colony", "Napur", " MH", "440034", "8149240833", "prajakta@gmail.com"));
+        addressBookDetailsList.add(new AddressBookDetails("Payal", "Shende", "Rukmini Nagar", "Wardha", "MH", "442001", "7385697450", "shende.samiksha@gmail.com"));
+        addressBookDetailsList.add(new AddressBookDetails("Ekta", "Shende", "GopalNagar", "Wardha", "MH", "442001", "8794569789", "ektashende12@gmail.com"));
         addressBookDetailsList.add(new AddressBookDetails("Neha", "Zade", "Gajanan Nagar", "Wardha", "MH", "442001", "9745073856", "neha.zade@gmail.com"));
-        List<AddressBookDetails> contactList = addressBookMain.addContactList(addressBookDetailsList);
-        Assertions.assertEquals(3, contactList.size());
+        HashMap<String, List<AddressBookDetails>> contactList = addressBookMain.addContactList("Family", addressBookDetailsList);
+        Assertions.assertEquals(4, contactList.get("Family").size());
+    }
+    @Test
+    public void whenNewAddressBookCreated_ShouldReturn_true()
+    {
+        AddressBookMain addressBookMain = new AddressBookMain();
+        HashMap<String, List<AddressBookDetails>> addressBookList = addressBookMain.createNewAddAddressBook("Friends");
+        Assertions.assertTrue(addressBookList.containsKey("Friends"));
     }
 }
