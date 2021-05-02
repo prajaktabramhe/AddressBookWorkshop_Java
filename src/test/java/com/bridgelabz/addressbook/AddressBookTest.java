7937e7e8c0c23726d15a57271b4333117a421d15
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-02 23:42:53 +0530

    [Prajakta]: Added: Passed the multple data to test case

--- src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
@@ -42,10 +42,13 @@ public class AddressBookTest
     public void givenListOfContacts_whenAdded_ShouldReturn_true() {
         AddressBookMain addressBookMain = new AddressBookMain();
         List<AddressBookDetails> addressBookDetailsList = new ArrayList<>();
+        addressBookDetailsList.add(new AddressBookDetails("Prajakta", "Bramhe", " Ashtyavinayak Colony", "Napur", " MH", "440034", "8149240833", "prajakta@gmail.com"));
         addressBookDetailsList.add(new AddressBookDetails("Samiksha", "Shende", "Ram Nagar", "Wardha", "MH", "442001", "7385697450", "shende.samiksha@gmail.com"));
-        addressBookDetailsList.add(new AddressBookDetails("Apurva", "Yede", "Murarka Layout", "Wardha", "MH", "442001", "8794569789", "apurvayede@gmail.com"));
+        addressBookDetailsList.add(new AddressBookDetails("Ekta", "Shende", "GopalNagar", "Wardha", "MH", "442001", "8794569789", "ektashende12@gmail.com"));
         addressBookDetailsList.add(new AddressBookDetails("Neha", "Zade", "Gajanan Nagar", "Wardha", "MH", "442001", "9745073856", "neha.zade@gmail.com"));
         List<AddressBookDetails> contactList = addressBookMain.addContactList(addressBookDetailsList);
-        Assertions.assertEquals(3, contactList.size());
+        Assertions.assertEquals(4, contactList.size());
     }
+
+
 }
