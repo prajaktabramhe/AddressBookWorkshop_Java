a7e0eb59c672d6fd0af85cfdbd80e870b19f8739
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-02 22:56:29 +0530

    [Prajakta]: passed the data for update

--- src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookTest.java
@@ -3,6 +3,8 @@ package com.bridgelabz.addressbook;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class AddressBookTest
 {
     @Test
@@ -10,7 +12,18 @@ public class AddressBookTest
     {
       AddressBookMain addressBookMain = new AddressBookMain();
       AddressBookDetails addressBookDetails = new AddressBookDetails("Prajakta", "Bramhe", "Ashtyavinayak colony", "Nagpur", "MH", "440034", "8149240833", "Prajkat@gmail.com");
-      boolean isAdded = addressBookMain.addContact(addressBookDetails);
-      Assertions.assertTrue(isAdded);
+
+        List<AddressBookDetails> addressBookDetailsList = addressBookMain.addContact(addressBookDetails);
+      Assertions.assertEquals(1, addressBookDetailsList.size());
+    }
+
+    @Test
+    public  void givenContactName_whenUpdated_shoouldReturn_TRUE()
+    {
+        AddressBookMain addressBookMain = new AddressBookMain();
+        AddressBookDetails addressBookDetails = new AddressBookDetails("Prajakta", "Bramhe", " Ashtyavinayak Colony", "Napur", " MH", "440034", "8149240833", "prajakta@gmail.com");
+        List<AddressBookDetails> addressBookDetailsList = addressBookMain.addContact(addressBookDetails);
+        AddressBookDetails updateContact = addressBookMain.updateContact(addressBookDetailsList, "Prajakta", "address", "Ganesh colony");
+        Assertions.assertEquals("Ganesh colony", updateContact.address);
     }
 }
